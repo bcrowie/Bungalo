d7d2f28ea453de398dbfcf5bc9bff216f8d4359b
Brenden Crowie <brenden@crowie.dev>
2023-02-28 13:13:26 -0500

    Added backend server

--- server/app.js
+++ server/app.js
@@ -6,3 +6,9 @@ const app = express()
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
+
+app.get('/containers', (req, res) => {
+    res.status(200).json({message: "Containers Here"})
+})
+
+
