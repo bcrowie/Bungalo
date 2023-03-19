38528073284f87cbe4733abd9560668855171cbb
Brenden Crowie <brendencrowie@gmail.com>
2023-03-19 19:04:18 -0400

    Remove menu from navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,48 +5,11 @@ import { mdiCog, mdiDocker, mdiMenu } from '@mdi/js'
 import '../styles/Navbar.scss'
 
 const Navbar: React.FC = () => {
-    const [menuExpanded, setMenuExpanded] = useState(false)
-
-    useEffect(() => {
-        if(menuExpanded){
-            document.addEventListener('mousedown', (e: MouseEvent) => {
-                if((e.target as Element).classList.value !== 'settings-menu'
-                    || (e.target as Element).classList.value !== 'menu-item'){
-                        setMenuExpanded(false)
-                }
-            })
-        }
-    }, [menuExpanded, setMenuExpanded])
-
     return (
     <>
         <div className="navbar">
             <h2>Bungalo</h2>
-            <button className="menu-button" onClick={() => setMenuExpanded(!menuExpanded)}>
-                <Icon path={mdiMenu} size={1} />
-            </button>
-            {menuExpanded && <div className="settings-menu">
-                    <ul>
-                        <li className="menu-item">
-                                <Icon path={mdiDocker} size={1}/>
-                                <Link className="page-link" to={'/'}>
-                                    Docker
-                                </Link>
-                        </li>
-                        <li className="menu-item">
-                                <Icon path={mdiCog} size={1}/>
-                                <Link className="page-link" to={'/settings'}>
-                                    Settings
-                                </Link>
-                        </li>
-                        <li className="menu-item">
-                            About
-                        </li>
-                        <li className="menu-item">
-                            Other
-                        </li>
-                    </ul>
-                </div>}
+            
         </div>
     </>
     )
